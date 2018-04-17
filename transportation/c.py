0eceee84dba20be633da0e04b2f2b59d1784032c
xz2631@columbia.edu <xz2631@columbia.edu>
2018-04-17 13:16:54 -0400

    10-11 delay

---
+++ transportation/c.py
@@ -0,0 +1,17 @@
+import sys
+import numpy as np
+import datetime
+import os
+import time
+'''
+while(1):
+	print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+	time.sleep(5)'''
+
+time_now= time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+print("year", time_now[:4])
+print("month", time_now[5:7])
+print("day", time_now[8:10])
+print("hour", time_now[11:13])
+print("minute", time_now[14:16])
+print("second", time_now[17:19])
